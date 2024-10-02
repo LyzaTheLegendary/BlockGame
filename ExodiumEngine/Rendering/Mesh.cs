f224930c3f1ff2afac63e0f04bd5cdba0dbf131c
Wienerman <Nem56@NEMO-WINDOWS>
2024-10-03 01:09:13 +0200

    Corrected project name

--- ExodiumEngine/Rendering/Mesh.cs
+++ ExodiumEngine/Rendering/Mesh.cs
@@ -6,30 +6,30 @@ namespace ExodiumEngine.Rendering
 {
     public readonly struct Mesh // idea make one ofthese which represents a normal block, and keep copying this one so we have the same gpu data.
     {
-        readonly int _indicesSize = 0;
-        readonly int _vboGraphicsPointer = 0;
-        readonly int _vboTexelGraphicsPointer = 0;
-        readonly int _iboGraphicsPointer = 0;
-        readonly int _vaoGraphicsPointer = 0;
+        readonly int m_indicesSize = 0;
+        readonly int m_vboGraphicsPointer = 0;
+        readonly int m_vboTexelGraphicsPointer = 0;
+        readonly int m_iboGraphicsPointer = 0;
+        readonly int m_vaoGraphicsPointer = 0;
 
         readonly Material m_material;
         // owns texture?
 
         //public Matrix4 transformation = Matrix4.CreateTranslation(0, 0, -3f); shouldn't be in here, as this is not the renderable object, it is simply the mesh
-        public readonly int VboPointer => _vboGraphicsPointer;
-        public readonly int VboTextPointer => _vboTexelGraphicsPointer;
-        public readonly int IboPointer => _iboGraphicsPointer;
-        public readonly int VaoPointer => _vaoGraphicsPointer;
-        public readonly int Indices => _indicesSize;
+        public readonly int VboPointer => m_vboGraphicsPointer;
+        public readonly int VboTextPointer => m_vboTexelGraphicsPointer;
+        public readonly int IboPointer => m_iboGraphicsPointer;
+        public readonly int VaoPointer => m_vaoGraphicsPointer;
+        public readonly int Indices => m_indicesSize;
         public Material Material => throw new NotImplementedException("Materials are not implemented yet!");
 
         public Mesh(int vboPointer, int vboTexelPointer, int iboPointer, int vaoPointer, int indicesSize)
         {
-            _indicesSize = indicesSize;
-            _vboGraphicsPointer = vboPointer;
-            _vboTexelGraphicsPointer = vboTexelPointer;
-            _iboGraphicsPointer = iboPointer;
-            _vaoGraphicsPointer = vaoPointer;
+            m_indicesSize = indicesSize;
+            m_vboGraphicsPointer = vboPointer;
+            m_vboTexelGraphicsPointer = vboTexelPointer;
+            m_iboGraphicsPointer = iboPointer;
+            m_vaoGraphicsPointer = vaoPointer;
         }
         public Mesh Clone() => (Mesh)MemberwiseClone();
 
